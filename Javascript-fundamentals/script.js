1eb799a9f1e9363c96ad6a2670ad19b90e97088b
Benjie Gil Angeles <126475746+thebenjieangeles@users.noreply.github.com>
2023-03-23 20:07:53 +0700

    Add files via upload

---
+++ Javascript-fundamentals/script.js
@@ -0,0 +1,82 @@
+// let grade = prompt("Grade");
+
+// if (grade >= 100) {
+//   console.log("With highest honors");
+// } else if (grade >= 98 && grade <= 100) {
+//   console.log("With highest honors");
+// } else if (grade >= 95 && grade <= 97) {
+//   console.log("With high honors");
+// } else if (grade >= 90 && grade <= 94) {
+//   console.log("With honors");
+// } else {
+//   console.log("Butaw");
+// }
+// 2;
+
+// let grade = prompt("What is your grade?");
+// switch (true) {
+//   case grade >= 90:
+//     console.log("A");
+//     break;
+
+//   case grade >= 80:
+//     console.log("B");
+//     break;
+
+//   case grade >= 70:
+//     console.log("C");
+//     break;
+//   case grade >= 60:
+//     console.log("D");
+//     break;
+//   default:
+//     console.log("F");
+// }
+
+/* 
+let month = 5;
+
+switch (month) {
+  case 0:
+  case 1:
+  case 2:
+    console.log("Winter");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Spring");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Summer");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("Autumn");
+    break;
+  default:
+    console.log("Something went wrong");
+}*/
+
+// function printMyName(name) {
+//   console.log("name");
+// }
+
+// printMyName("Benjie Angeles");
+
+// const random = (number) => {
+//   return Math.floor(Math.random() * number);
+// };
+
+// console.log(random(5));
+
+const isTodayMonday = () => {
+  return Math.floor(Math.random() * number);
+  const today = new Date();
+  const dayOfWeek = today.getDay(1);
+};
+
+console.log(isTodayMonday);
